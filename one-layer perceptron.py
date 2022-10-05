92637068100a1f5a8ad2b921e76306ee25d5a1fb
gustavwww <gustav.wadstrom@telia.com>
2022-10-05 19:41:14 +0200

    Working files for C value under 0.12

--- one-layer perceptron.py
+++ one-layer perceptron.py
@@ -2,14 +2,15 @@ import math
 
 import pandas as pd
 import numpy as np
+import random as rnd
 
 class Network:
 
     def __init__(self, M, learning_rate):
         self.M = M
         self.ln = learning_rate
-        self.w_1 = np.zeros((M, 2))
-        self.w_2 = np.zeros(M)
+        self.w_1 = 0.1*(np.random.normal(0, 30, size=(M, 2)))
+        self.w_2 = 0.1*(np.random.normal(0, 30, size=M))
         self.thetaV = np.zeros(M)
         self.thetaO = 0
         self.v = np.zeros(M)
@@ -52,30 +53,28 @@ class Network:
         return C
 
 
-    def trainNetwork(self, data, t):
-        for p in range(0, len(data)):
+    def trainNetwork(self, data, t, k):
 
+        for iter in range(0, k):
+            print(iter)
+            p = rnd.randint(0, len(data)-1)
             output, b_o = self.forwardIterate(data[p])
 
-            # Update weights and thresholds with backpropagation
-
-            # Update for w_2
+            # Calc update for w_2 and pattern p
             error_w2 = (t[p] - output) * 1/pow(math.cosh(b_o), 2)
+            self.thetaO += -self.ln * error_w2
+
             for i in range(0, self.M):
                 dw_2 = self.ln * error_w2 * self.v[i]
                 self.w_2[i] += dw_2
 
-            d_theta = -self.ln * error_w2
-            self.thetaO += d_theta
-
-            # Update for w_1
+            # Calc update for w_1 and pattern p
             for i in range(0, self.M):
                 error_w1_i = error_w2 * self.w_2[i] * 1/pow(math.cosh(self.b_v[i]), 2)
-                d_theta = -self.ln * error_w1_i
-                self.thetaV[i] += d_theta
+                self.thetaV[i] += -self.ln * error_w1_i
 
                 for j in range(0, 2):
-                    dw_1 = error_w1_i * data[p][j]
+                    dw_1 = self.ln * error_w1_i * data[p][j]
                     self.w_1[i][j] += dw_1
 
 
@@ -88,11 +87,20 @@ t_training = training.iloc[:, 2].to_numpy()
 data_validation = validation.iloc[:, [0,1]].to_numpy()
 t_validation = validation.iloc[:, 2].to_numpy()
 
-M = 1
-learning_rate = 0.5
+M = 10
+learning_rate = 0.03
+
 network = Network(M, learning_rate)
 
-network.trainNetwork(data_training, t_training)
-C = network.validate(data_validation, t_validation)
+C = 1
+while C >= 0.12:
+    network.trainNetwork(data_training, t_training, 10000)
+    C = network.validate(data_validation, t_validation)
+    print("Epoch finished: C = " + str(C))
+
+pd.DataFrame(network.w_1).to_csv("files/w1.csv")
+pd.DataFrame(network.w_2).to_csv("files/w2.csv")
+pd.DataFrame(network.thetaV).to_csv("files/t1.csv")
+pd.DataFrame([network.thetaO]).to_csv("files/t2.csv")
 
 print(C)
